6116dc5ea10ee41f642ef3b9648998fdf5cc87c6
Jalaj <96870071+jalaj711@users.noreply.github.com>
2023-12-16 19:13:50 +0530

    added function to perform substitution from s-boxes
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

--- round_function.go
+++ round_function.go
@@ -15,3 +15,27 @@ func expand(input [4]byte) (output [6]byte) {
 	}
 	return output
 }
+
+// get_bit_at_position takes a 48-bit byte array and returns the bit at position pos (0-indexed)
+func get_bit_at_position(input [6]byte, pos int) byte {
+	// pos was 0-indexed we convert it to 1-indexed
+	pos++
+	if pos%8 == 0 {
+		return input[(pos-1)/8] & 1
+	}
+	return (input[(pos-1)/8] >> (8 - pos%8)) & 1
+}
+
+// substituteFromSBox performs the substitution from S-Boxes
+func substituteFromSBox(input [6]byte) (output [4]byte) {
+	var temp byte = 0
+	for i := 0; i < 48; i += 6 {
+		temp = (temp << 4) | S_BOXES[i/6][(get_bit_at_position(input, i)<<1)|
+			get_bit_at_position(input, i+5)][get_bit_at_position(input, i+1)<<3|
+			get_bit_at_position(input, i+2)<<2|
+			get_bit_at_position(input, i+3)<<1|
+			get_bit_at_position(input, i+4)]
+		output[i/12] = temp
+	}
+	return output
+}
--- round_function_test.go
+++ round_function_test.go
@@ -13,3 +13,13 @@ func TestExpansionFuncton(t *testing.T) {
 		t.Fatalf(`Testcase: %b, result: %b, expected: %b`, test, key, expected)
 	}
 }
+
+func TestSBoxSubstitution(t *testing.T) {
+	test := [6]byte{0b01100001, 0b00010111, 0b10111010, 0b10000110, 0b01100101, 0b00100111}
+	expected := [4]byte{0b01011100, 0b10000010, 0b10110101, 0b10010111}
+	key := substituteFromSBox(test)
+	// t.Logf(`Testcase: %b, result: %d`, test, key)
+	if key != expected {
+		t.Fatalf(`Testcase: %b, result: %b, expected: %b`, test, key, expected)
+	}
+}
