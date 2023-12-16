206ad37992e22025ba7b83538dbfe5a0086d6349
Jalaj <96870071+jalaj711@users.noreply.github.com>
2023-12-16 19:48:23 +0530

    added  Encypt64 function to encrypt 64-bit block of data
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

---
+++ des.go
@@ -0,0 +1,32 @@
+package godes
+
+// Encrypt64 takes 64-bit data block and encrypts it using a 64-bit key by applying DES
+func Encrypt64(data [8]byte, key [8]byte) (encrypted [8]byte) {
+	round_keys := getRoundKeys(key)
+
+	// IP
+	data = initial_permutation(data)
+
+	var L0, R0, L1, R1, out [4]byte
+	L0 = [4]byte{data[0], data[1], data[2], data[3]}
+	R0 = [4]byte{data[4], data[5], data[6], data[7]}
+	for i := 0; i < 16; i++ {
+		// Basically thorugh every round we do
+		// L(i) = R(i-1)
+		// R(i) = L(i-1) XOR F(R(i-1), ROUNDKEY)
+		L1 = R0
+		out = round_function(R0, round_keys[i])
+		for j := 0; j < 4; j++ {
+			R1[j] = L0[j] ^ out[j]
+		}
+		L0 = L1
+		R0 = R1
+	}
+
+	// 32-bit swap
+	encrypted = [8]byte{R0[0], R0[1], R0[2], R0[3], L0[0], L0[1], L0[2], L0[3]}
+
+	// IP-1
+	encrypted = inverse_initial_permutation(encrypted)
+	return encrypted
+}
---
+++ des_test.go
@@ -0,0 +1,15 @@
+package godes
+
+import (
+	"testing"
+)
+
+func TestEncrypt64(t *testing.T) {
+	test_key := [8]byte{0b00010011, 0b00110100, 0b01010111, 0b01111001, 0b10011011, 0b10111100, 0b11011111, 0b11110001}
+	test_data := [8]byte{0b00000001, 0b00100011, 0b01000101, 0b01100111, 0b10001001, 0b10101011, 0b11001101, 0b11101111}
+	expected := [8]byte{0b10000101, 0b11101000, 0b00010011, 0b01010100, 0b00001111, 0b00001010, 0b10110100, 0b00000101}
+	encrypted := Encrypt64(test_data, test_key)
+	if encrypted != expected {
+		t.Fatalf(`Testcase: data=%b;key=%b, result: %b, expected: %b`, test_data, test_key, encrypted, expected)
+	}
+}
