1532aa5d411777871df10a388684970e2f599bce
Jalaj <96870071+jalaj711@users.noreply.github.com>
2023-12-16 13:21:34 +0530

    added function to left rotate keys
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

--- keygen.go
+++ keygen.go
@@ -9,3 +9,42 @@ func getReducedKey(key [8]byte) [7]byte {
 	}
 	return reduced
 }
+
+// leftRotateKey takes a 56-bit key in the format of a byte array by rotationAmount
+// It does so in 2 groups, ie the key is divided into two groups of 28-bits each
+// and both groups are rotated seperately
+func leftRotateKey(key [7]byte, rotationAmount uint8) (rotatedKey [7]byte) {
+
+	// stores the first bit(s) of input to be concatenated to the last of shifted bits
+	lastOutputBits := (key[0] >> (8 - rotationAmount))
+
+	var temp byte
+	var i byte
+
+	// stores as many ones as the rotationAmount
+	// used to extract bits from a byte
+	var allOnes byte = 0
+	for i = rotationAmount; i > 0; i-- {
+		allOnes = (allOnes << 1) | 1
+	}
+
+	// left shifts bit 1 to bit (28-rotationAmount)
+	for i = 0; i < 28-rotationAmount; i++ {
+		temp = (temp << 1) | ((key[(i+rotationAmount)/8] >> (7 - (i+rotationAmount)%8)) & 1)
+		rotatedKey[i/8] = temp
+	}
+	// sets last bits of this rotation
+	temp = (temp << rotationAmount) | lastOutputBits
+	rotatedKey[3] = temp
+
+	// update output bits for second half
+	lastOutputBits = (key[3] >> (4 - rotationAmount)) & allOnes
+	for i = 28; i < 56-rotationAmount; i++ {
+		temp = (temp << 1) | ((key[(i+rotationAmount)/8] >> (7 - (i+rotationAmount)%8)) & 1)
+		rotatedKey[i/8] = temp
+	}
+	temp = (temp << rotationAmount) | lastOutputBits
+	rotatedKey[6] = temp
+
+	return rotatedKey
+}
--- keygen_test.go
+++ keygen_test.go
@@ -13,3 +13,23 @@ func TestGetReducedKey(t *testing.T) {
 		t.Fatalf(`Testcase: %b, result: %b`, test, key)
 	}
 }
+
+func TestLeftRotateKeySingleShift(t *testing.T) {
+	test := [7]byte{0b11110000, 0b11001100, 0b10101010, 0b11110101, 0b01010110, 0b01100111, 0b10001111}
+	key := leftRotateKey(test, 1)
+	expected := [7]byte{0b11100001, 0b10011001, 0b01010101, 0b11111010, 0b10101100, 0b11001111, 0b00011110}
+	// t.Logf(`Testcase: %b, result: %d`, test, key)
+	if key != expected {
+		t.Fatalf(`Testcase: %b result: %b\n expected: %b`, test, key, expected)
+	}
+}
+
+func TestLeftRotateKeyDoubleShift(t *testing.T) {
+	test := [7]byte{0b11000011, 0b00110010, 0b10101011, 0b11110101, 0b01011001, 0b10011110, 0b00111101}
+	key := leftRotateKey(test, 2)
+	expected := [7]byte{0b00001100, 0b11001010, 0b10101111, 0b11110101, 0b01100110, 0b01111000, 0b11110101}
+	// t.Logf(`Testcase: %b, result: %d`, test, key)
+	if key != expected {
+		t.Fatalf(`Testcase: %b result: %b\n expected: %b`, test, key, expected)
+	}
+}
